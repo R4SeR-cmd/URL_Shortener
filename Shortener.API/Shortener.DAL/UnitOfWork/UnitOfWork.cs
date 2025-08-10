e1c4c7c97bffb51b93f670efba822e9eca9a0ed3
R4SeR-cmd <72449672+R4SeR-cmd@users.noreply.github.com>
2025-08-10 23:22:55 +0300

    Create UnitOfWork.cs

---
+++ Shortener.API/Shortener.DAL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using Shortener.DAL.Context;
+using Shortener.DAL.Repositories.Interfaces;
+using Shortener.DAL.UnitOfWork.Interfaces;
+
+namespace Shortener.DAL.UnitOfWork
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private readonly ShortenerDbContext _context;
+
+		public IUserRepository Users { get; }
+		public IShortUrlRepository ShortUrls { get; }
+
+		public UnitOfWork(
+			ShortenerDbContext context,
+			IUserRepository userRepository,
+			IShortUrlRepository shortUrlRepository)
+		{
+			_context = context;
+			Users = userRepository;
+			ShortUrls = shortUrlRepository;
+		}
+
+		public async Task<int> SaveChangesAsync()
+		{
+			return await _context.SaveChangesAsync();
+		}
+	}
+}
